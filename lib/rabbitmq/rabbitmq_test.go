e82d46585a0edcae86c9ea489a329f500a6e5aea
jt <1293628336@qq.com>
2023-02-06 14:20:52 +0800

    RabbitMQ 测试

--- lib/rabbitmq/rabbitmq_test.go
+++ lib/rabbitmq/rabbitmq_test.go
@@ -1,11 +1,78 @@
 package rabbitmq
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
-const host = "amqp://test:test@192.168.1.5:5672"
+const host = "amqp://guest:guest@172.24.170.109:5672"
 
-func TestRabbitMQ_Publish(t *testing.T) {
+func TestPublish(t *testing.T) {
 	queue := New(host)
 	defer queue.Close()
 	queue.Bind("test")
+
+	queue2 := New(host)
+	defer queue.Close()
+	queue.Bind("test")
+
+	queue3 := New(host)
+	defer queue.Close()
+
+	expect := "test"
+	queue3.Publish("test2", "any")
+	queue3.Publish("test", expect)
+
+	consume := queue.Consume()
+	message := <-consume
+	var actual interface{}
+	err := json.Unmarshal(message.Body, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual != expect {
+		t.Errorf("expected %s,actual %s", expect, actual)
+	}
+	if message.ReplyTo != queue3.Name {
+		t.Error(message)
+	}
+	queue2.Send(message.ReplyTo, "test3")
+	consume3 := queue3.Consume()
+	message = <-consume3
+	if string(message.Body) != `"test3"` {
+		t.Error(string(message.Body))
+	}
+}
+
+func TestSend(t *testing.T) {
+	queue := New(host)
+	defer queue.Close()
+
+	queue2 := New(host)
+	defer queue2.Close()
+
+	expect := "test"
+	expect2 := "test2"
+	queue.Send(queue.Name, expect)
+	queue.Send(queue2.Name, expect2)
+
+	consume := queue.Consume()
+	message := <-consume
+	var actual interface{}
+	err := json.Unmarshal(message.Body, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual != expect {
+		t.Errorf("expected %s ,actual %s", expect, actual)
+	}
+	consume2 := queue2.Consume()
+	message = <-consume2
+	err = json.Unmarshal(message.Body, &actual)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual != expect2 {
+		t.Errorf("expected %s ,actual %s", expect, actual)
+	}
 }
--- part2/test/test.sh
+++ part2/test/test.sh
@@ -2,6 +2,6 @@
 
 curl -v 172.24.160.1:8800/objests/test2 -XPUT -d "this is object test2"
 
-curl 172.24.160.1:8800/objests/test2
+curl 172.24.160.1:8800/locate/test2
 echo
-curl 172.24.160.1:8800/objests/test2
+curl 172.24.160.1:8800/objects/test2
